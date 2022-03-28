5b47f8d9c0acff39e4497c108b0fc8dcf419ca75
김민제 <mu8786@gmail.com>
2022-03-28 20:57:55 +0900

    시작

---
+++ src/test/java/com/mj/craftpractise/CraftpractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mj.craftpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftpractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
